babc8bc9b7ca2975bef8f63d597a4e232acfe7fe
lokkwing <lokkwing@gmail.com>
2020-06-29 17:12:42 +0800

    .
    .

---
+++ Walk Counter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Walk Counter
+//
+//  Created by Ohkei Nie on 11/6/20.
+//  Copyright © 2020 Ohkei N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
